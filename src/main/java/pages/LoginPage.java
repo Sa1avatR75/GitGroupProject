4503298465a042ff2e72d4ad5d772b832c71e3a0
imanchik03 <imanurinbaev056@gmail.com>
2024-07-18 20:17:25 -0500

    login page added

---
+++ src/main/java/pages/LoginPage.java
@@ -0,0 +1,32 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+
+    public class LoginPage {
+        WebDriver driver;
+
+        public LoginPage(WebDriver driver  ) {
+            this.driver = driver;
+            PageFactory.initElements(driver, this);
+        }
+        @FindBy(id ="user-name")
+        public WebElement username;
+
+        @FindBy(id="password")
+        public WebElement passwordInput;
+
+        @FindBy(id="login-button")
+        public WebElement loginButton;
+
+        public void testLogin(String name, String password){
+            username.sendKeys(name);
+            passwordInput.sendKeys(password);
+            loginButton.click();
+
+        }
+    }
+
